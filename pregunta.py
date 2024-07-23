08868469be6eb05f921d4bf201d1c7e8e82fa9ed
Kevin Guio <kguio@unal.edu.co>
2024-07-22 19:18:40 -0500

    Corrección

--- pregunta.py
+++ pregunta.py
@@ -10,55 +10,92 @@ import pandas as pd
 
 
 
+def limpieza_columna_sexo(df):
+    df = df.copy()
+    df.sexo = (df.sexo
+               .str.lower()
+               .str.replace("femenino", "f")
+               .str.replace("masculino", "m"))
+    
+    return df
+
+def limpieza_columna_tipo_de_emprendimiento(df):
+    df = df.copy()
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    
+    return df
+
+def limpieza_columna_idea_negocio(df):
+    df = df.copy()
+    df.idea_negocio = (df.idea_negocio
+                       .str.lower()
+                       .str.replace('_', ' ')
+                       .str.replace('-', ' ')
+                       .str.replace('.', ''))
+    return df
+
+def limpieza_columna_barrio(df):
+    df = df.copy()
+    df.barrio = (df.barrio
+                 .str.lower()
+                 .str.replace('_', ' ')
+                 .str.replace('-', ' '))
+    return df
+
+def limpieza_columna_estrato(df):
+    df = df.copy()
+    df.estrato = df.estrato.map(int)
+    return df
+
+def limpieza_columna_comuna_ciudadano(df):
+    df = df.copy()
+    df.comuna_ciudadano = df.comuna_ciudadano.map(int)
+    return df
+
+def limpieza_columna_fecha_de_beneficio(df):
+    df = df.copy()
+    df.fecha_de_beneficio = (pd.to_datetime(df.fecha_de_beneficio, format="%d/%m/%Y", errors = "coerce")
+                             .fillna(pd.to_datetime(df.fecha_de_beneficio, format="%Y/%m/%d", errors="coerce")))
+    return df
+
+def limpieza_columna_monto_del_credito(df):
+    df = df.copy()
+    df.monto_del_credito = (df.monto_del_credito
+                            .str.replace("$", "")
+                            .str.strip()
+                            .str.replace(",", "")
+                            .map(lambda x: int(x.split(".")[0])))
+    return df
+
+def limpieza_columna_línea_credito(df):
+    df = df.copy()
+    df.línea_credito = df.línea_credito.str.lower()
+    return df
+
+
+
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
-    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    df_limpio = df.copy()
 
-    # QUITAR LOS NAN
+    
+    df_limpio = limpieza_columna_sexo(df_limpio)
+    df_limpio = limpieza_columna_tipo_de_emprendimiento(df_limpio)
+    df_limpio = limpieza_columna_idea_negocio(df_limpio)
+    df_limpio = limpieza_columna_barrio(df_limpio)
+    df_limpio = limpieza_columna_estrato(df_limpio)
+    df_limpio = limpieza_columna_comuna_ciudadano(df_limpio)
+    df_limpio = limpieza_columna_fecha_de_beneficio(df_limpio)
+    df_limpio = limpieza_columna_monto_del_credito(df_limpio)
+    df_limpio = limpieza_columna_línea_credito(df_limpio)
 
+    # Eliminar duplicados
+    df_limpio = df_limpio.drop_duplicates()
 
-    # LIMPIEZA COLUMNA SEXO
-    df.sexo = df.sexo.str.lower()
+    # Eliminar filas con datos faltantes
+    df_limpio = df_limpio.dropna()
 
-    #LIMPIEZA COLUMNA TIPO_DE_EMPRENDIMIENTO
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    return(df_limpio)
 
-    # QUITAR COLUMNA UNNAMED
-
-
-    # LIMPIEZA COLUMNA idea_negocio
-    df.idea_negocio = (df.idea_negocio.str.lower().str.replace("_"," ").
-                       str.replace("-"," ").str.strip())
-
-    #LIMPIEZA COLUMNA BARRIO
-    df.barrio = df.barrio.str.lower()
-
-    #LIMPIEZA COLUMNA ESTRATO
-
-    #LIMPIEZA COLUMNA comuna_ciudadano
-
-    df.comuna_ciudadano = df.comuna_ciudadano.map(int)
-
-    #LIMPIEZA COLUMNA FECHA DE BENEFICIO
-
-    df.fecha_de_beneficio = (pd.to_datetime(df.fecha_de_beneficio, format="%d/%m/%Y", errors = "coerce").
-                             fillna(pd.to_datetime(df.fecha_de_beneficio, format="%Y/%m/%d", errors="coerce")))
-
-    #LIMPIEZA COLUMNA MONTO DEL CRÉDITO
-
-    df.monto_del_credito = (df.monto_del_credito.str.replace("$","").str.strip().str.replace(",","").
-                            map(lambda x: float(x.split(".")[0])))
-
-    #LIMPIEZA LÍNEA CRÉDITO
-    df.línea_credito = df.línea_credito.str.lower()
-
-    df = df.drop_duplicates().dropna()
-
-    #
-    # Inserte su código aquí
-
-
-
-    return df
-
+print(clean_data().head(10))
